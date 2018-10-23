7f18725c1e8436bc84c404f46922531e5e5e1f31
Skara <sk.aravind.93@gmail.com>
2018-10-23 21:36:20 +0800

    merge

--- week4/blockchain.py
+++ week4/blockchain.py
@@ -1,18 +1,23 @@
 from block import Block
 import time
-import json
 import ecdsa
+import json
+from transaction import Transaction
 
 class Blockchain:
     def __init__(self):
+        self.master_private_key = ecdsa.SigningKey.generate(curve=ecdsa.NIST192p)
+        self.master_public_key = self.master_private_key.get_verifying_key()
+        self.genesis_private_key = ecdsa.SigningKey.generate(curve=ecdsa.NIST192p)
+        self.genesis_public_key = self.genesis_private_key.get_verifying_key()
         self.chain=[self.createGenesisBlock()]
         self.pendingtransaction=[]
         self.miningReward=100
-        self.master_private_key = ecdsa.SigningKey.generate(curve=ecdsa.NIST192p)
-        self.master_public_key = self.master_private_key.get_verifying_key()
+        
+        
 
     def createGenesisBlock(self):
-        trans1 = Transaction('genesis_sender',self.master_public_key.to_string(),100000)
+        trans1 = Transaction(self.genesis_public_key.to_string(),self.master_public_key.to_string(),100000)
         pendingtransaction=[trans1]
         return Block("01/09/2018", pendingtransaction, "0")
 
@@ -23,7 +28,7 @@ class Blockchain:
         fork = False
         if len(self.chain) > index:
             fork = True
-        check = checkvalidblock(block,index,fork)
+        check = self.checkvalidblock(block,index,fork)
         if check == True:
             for b in block:
                 for trans in b.transactions:
@@ -33,7 +38,7 @@ class Blockchain:
             reward.sign(reward.json_msg, self.master_private_key.to_string())
             self.pendingtransaction.append(reward)
         if fork==True:
-            resolve()
+            self.resolve()
         
     def resolve(self):
         if len(self.chain[-1][0]) > len (self.chain[-1][1]):
@@ -47,7 +52,25 @@ class Blockchain:
             for i in self.chain[-1][1]:
                 new_chain.append(i)
             self.chain = new_chain[:]
-    
+            
+    def checkvalidblock(self,block_array,block_index,fork):
+        firstBlock = block_array[0]
+        previousBlock = self.chain[block_index-1]
+        if firstBlock.previousHash == previousBlock.hash:
+            if fork:
+                last_block=self.chain.pop()
+                forked_block = [last_block,block_array]
+                self.chain.append(forked_block)
+            else:
+                self.chain.append(block_array)
+
+            return True
+        else:
+            print ("Block Rejected")
+            return False 
+
+
+
 
 
 
